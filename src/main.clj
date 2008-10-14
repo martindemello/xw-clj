3dcafc7059b36daea18e233bf6950c90afad4087
Martin DeMello <martindemello@gmail.com>
2008-10-13 19:07:15 -0700

    drawing a basic grid

--- src/main.clj
+++ src/main.clj
@@ -1,6 +1,15 @@
 (in-ns 'main)
 (clojure/refer 'clojure)
 
+(import 
+  '(java.awt BasicStroke Color Dimension Graphics Graphics2D RenderingHints)
+  '(java.awt.geom AffineTransform Ellipse2D FlatteningPathIterator GeneralPath
+                 Line2D PathIterator Point2D) 
+  '(java.awt.image BufferedImage)
+  '(java.awt.event WindowAdapter WindowEvent)
+  '(java.awt.font TextLayout FontRenderContext)
+  '(javax.swing JFrame JPanel))
+
 (def N 10)
 (def board (hash-map))
 
@@ -39,9 +48,34 @@
 (doseq [i j] board-iter
   (def board (assoc board [i j] [" " nil])))
 
-(defn main [args]
-  (renumber)
-  (doseq i (range N)
-    (doseq j (range N)
-      (print (board [i j])))
-    (prn)))
+(def board (assoc board [0 9] [:black nil]))
+
+; Graphics
+
+  '(java.awt.font TextLayout FontRenderContext)
+
+(def X 800)
+(def Y 800)
+
+(defn render [g]
+  (let [img (new BufferedImage X Y (. BufferedImage TYPE_INT_ARGB))
+        bg (. img getGraphics)]
+    (. bg (setColor (. Color black)))
+    (let [n 600]
+      (doseq i (range 0 (+ n 40) 40)
+        (. bg drawLine 0 i n i)
+        (. bg drawLine i 0 i n)))
+
+    (. g (drawImage img 0 0 nil))
+    (. bg (dispose))))
+
+
+(def panel (doto (proxy [JPanel] [] (paint [g] (render g)))
+             (setBackground (. Color white))
+             (setPreferredSize (new Dimension X Y))))
+
+(def frame
+  (doto (new JFrame "xwe")
+    (add panel) (pack) (show)
+    (addWindowListener
+      (proxy [WindowAdapter] [] (windowClosing [e] (. System exit 0))))))
