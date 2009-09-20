26155ef93374490e23c56b46bfbc69318f822bca
Martin DeMello <martindemello@gmail.com>
2009-09-20 21:26:24 +0530

    clean up directory structure

--- src/clj-completions.clj
+++
@@ -1,7 +0,0 @@
-(defmacro with-out-file [pathname & body]
-  `(with-open stream# (new java.io.FileWriter ~pathname)
-     (binding [*out* stream#]
-       ~@body)))
-
-(def completions (keys (ns-publics (find-ns 'clojure))))
-(with-out-file "clj-keys.txt" (doseq x completions (println x)))
