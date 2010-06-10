b6ab885b89521537c4b0fd0b35cb486b374c5f5e
Martin DeMello <martindemello@gmail.com>
2010-06-10 19:01:56 +0530

    update to miglayout 3.7.3.1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [org.clojure/clojure-contrib "1.1.0"]
                  [commons-io "1.2"]
                  [commons-cli "1.1"]
-                 [com.miglayout/miglayout "3.7.2"]]
+                 [com.miglayout/miglayout "3.7.3.1"]]
   :main xw.main
   :namespaces [xw.main])
  
