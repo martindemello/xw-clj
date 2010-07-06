2c4fd634cedbc1d89dd595cce27221531f2d1012
Martin DeMello <martindemello@gmail.com>
2010-07-06 17:13:38 +0530

    upgrade to clojure 1.2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject xw-clj "1.0"
   :description "crossword editor"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [commons-io "1.2"]
                  [commons-cli "1.1"]
                  [com.h2database/h2 "1.2.137"]
