7f2dda0cee9a253546d8cfeddf5c58095c60ab76
Martin DeMello <martindemello@gmail.com>
2011-02-25 01:16:42 +0530

    factor out global word functions into their own file

--- src/clj/xw/cursor.clj
+++ src/clj/xw/cursor.clj
@@ -1,5 +1,5 @@
 (ns xw.cursor
-  (:use (xw board)))
+  (:use (xw board words)))
 
 (require '[clojure.contrib.str-utils2 :as s])
  
@@ -39,42 +39,6 @@
   (set-letter current-x current-y c))
 
 ; current word/pattern
-(defn re-char [i j]
-  (let [l (letter i j)]
-    (cond
-      (= l :empty) "."
-      (= l :black) "#"
-      true l)))
-
-(defn word-boundary? [i j]
-  (let [M (- N 1)]
-    (or (black? i j) (< i 0) (< j 0) (> i M) (> j M))))
-
-(defn in-word? [i j]
-  (not (word-boundary? i j)))
-
-(defn collect-ac [s j]
-  (map (fn [k] [k j]) (take-while #(in-word? % j) s)))
-
-(defn collect-dn [i s]
-  (map (fn [k] [i k]) (take-while #(in-word? i %) s)))
-
-(defn ac-word [i j]
-  (let [l (reverse (range 0 i))
-        r (range i N)]
-    (concat (reverse (collect-ac l j)) (collect-ac r j))))
-
-(defn dn-word [i j]
-  (let [u (reverse (range 0 j))
-        d (range j N)]
-    (concat (reverse (collect-dn i u)) (collect-dn i d))))
-
-(defn word-squares [x y dir]
-  (cond
-    (black? x y) nil
-    (= dir :across) (ac-word x y)
-    true (dn-word x y)))
-
 (defn current-word-squares []
   (word-squares current-x current-y current-dir))
 
@@ -84,19 +48,6 @@
 (defn set-current-word [w]
   (doseq [[i j l] (map conj (current-word-squares) w)]
     (set-letter i j (str l))))
-
-(defn crossing-word [x y dir]
-  (if (= dir :across)
-    (dn-word x y)
-    (ac-word x y)))
-
-(defn delete-word [x y dir]
-  (doseq [[i j] (word-squares x y dir)]
-    (let [ws (crossing-word i j dir)
-          empty? (fn [i] (apply blank? i))]
-      (when (or (= 1 (count ws))
-                (some empty? ws))
-        (set-letter i j :empty)))))
 
 (defn delete-current-word []
   (let [x current-x y current-y dir current-dir]
